966135759c15627bcd4cf3844c4c6df1e71492eb
Nirbhay <nirbhay.ram@lyearn.com>
2020-11-15 23:11:18 +0530

    project restructure

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import Twitter from './src/app/twitter/components/index';
+import Twitter from './src/app/module/twitter/components/index';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Twitter);
